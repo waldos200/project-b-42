0db0e5ef7a419389870889e982e80508b6bf0671
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-07-03 20:34:57 -0600

    usamos bcrypt para la contraseña del usuario

--- models/User.js
+++ models/User.js
@@ -1,4 +1,9 @@
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable func-names */
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const SALT_WORK_FACTOR = 10;
 
 const { Schema } = mongoose;
 
@@ -27,6 +32,26 @@ const userSchema = new Schema({
 }, {
   timestamps: true,
   versionKey: false,
+});
+userSchema.pre('save', function (next) {
+  const user = this;
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+
+  // generate a salt
+  return bcrypt.genSalt(SALT_WORK_FACTOR, function (errSalt, salt) {
+    if (errSalt) return next(errSalt);
+
+    // hash the password using our new salt
+    return bcrypt.hash(user.password, salt, function (errHash, hash) {
+      if (errHash) return next(errHash);
+
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      return next();
+    });
+  });
 });
 
 const User = mongoose.model('User', userSchema, 'Users');
