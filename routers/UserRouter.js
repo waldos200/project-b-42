6bdf60f0320204d8bd2700dc3d4d9275dcbd0d41
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-07-03 19:07:57 -0600

    agregar validacion al enpoint de creacion de usuario

--- routers/UserRouter.js
+++ routers/UserRouter.js
@@ -1,8 +1,10 @@
 const express = require('express');
+
+const { UserValidator } = require('../validators');
 const { UserController } = require('../controllers');
 
 const router = express.Router();
 
-router.post('/api/v1/users', UserController.create);
+router.post('/api/v1/users', UserValidator.create, UserController.create);
 
 module.exports = router;
