1e9b7270a6cdb43bd38bbee19a2b807822f5df19
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-07-03 19:08:18 -0600

    agregar validacion de errores

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { errors } = require('celebrate');
 
 const api = express();
 const PORT = process.env.PORT || 3000;
@@ -9,5 +10,7 @@ api.use(express.json({ extends: true }));
 api.get('/', (req, res) => res.send('hello word'));
 
 api.use(require('../routers'));
+
+api.use(errors());
 
 module.exports = { api, PORT };
