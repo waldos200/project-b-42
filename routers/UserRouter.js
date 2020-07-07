c915a1d24320fbca65f089aa20822cde4edb5be5
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-07-06 18:30:40 -0600

    agregue punto y coma

--- routers/UserRouter.js
+++ routers/UserRouter.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.post('/users', UserValidator.create, UserController.create);
 router.get('/users', UserController.findAll);
-router.get('/users/:id', UserValidator.findOne, UserController.findOne)
+router.get('/users/:id', UserValidator.findOne, UserController.findOne);
 router.patch('/users/:id', UserValidator.updateOne, UserController.updateOne);
 router.delete('/users/:id', UserValidator.deleteOne, UserController.deleteOne);
 
