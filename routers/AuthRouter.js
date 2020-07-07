c309cf2ba21e8daab80b87ba38bcfc4db67eb007
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-07-06 20:14:09 -0600

    corregi la palabra signup

--- routers/AuthRouter.js
+++ routers/AuthRouter.js
@@ -5,8 +5,8 @@ const { UserController } = require('../controllers');
 
 const router = express.Router();
 
-router.post('/singup',
-  UserValidator.create, UserController.singup);
+router.post('/signup',
+  UserValidator.create, UserController.signup);
 router.post('/login',
   UserValidator.login, UserController.login);
 
