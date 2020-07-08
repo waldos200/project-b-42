d2e272ced0be220109963022a26df429c6edfe9a
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-07-07 20:56:49 -0600

    agregar rutas privadas por medio de la verificacion del token

--- routers/index.js
+++ routers/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 
 const router = express.Router();
+const { verifyToken } = require('../middlewares');
 
+// Public
 router.use(require('./UserRouter'));
 router.use(require('./AuthRouter'));
+
+// Private
+router.use(verifyToken);
+router.use(require('./PostRouter'));
 
 module.exports = router;
