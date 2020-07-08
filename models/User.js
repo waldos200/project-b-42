8770a256c28b9c4c3fdf26ac081d9bf8abec9fac
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-07-07 18:46:55 -0600

    agregue ejemplos de referencias y embebidos

---
+++ models/User.js
@@ -0,0 +1,71 @@
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable func-names */
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const SALT_WORK_FACTOR = 10;
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  first_name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  /* // Ejemplo de REFERENCIA
+  author_of_references: [{
+    type: mongoose.Types.ObjectId,
+    ref: 'Books',
+  }],
+  // Ejemplo de EMBEBIDOS
+  author_of_embeded: [{
+    author: { type: String },
+    name: { type: String },
+    idbn: { type: String },
+    cover_art: { type: String },
+  }], */
+}, {
+  timestamps: true,
+  versionKey: false,
+});
+userSchema.pre('save', function (next) {
+  const user = this;
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+
+  // generate a salt
+  return bcrypt.genSalt(SALT_WORK_FACTOR, function (errSalt, salt) {
+    if (errSalt) return next(errSalt);
+
+    // hash the password using our new salt
+    return bcrypt.hash(user.password, salt, function (errHash, hash) {
+      if (errHash) return next(errHash);
+
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      return next();
+    });
+  });
+});
+
+const User = mongoose.model('User', userSchema, 'Users');
+
+module.exports = User;
