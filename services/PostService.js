bd9cbce261d5b6f699102ce2d41e12742d81022a
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-07-08 19:33:02 -0600

    agregar end points

--- services/PostService.js
+++ services/PostService.js
@@ -2,4 +2,21 @@ const { Post } = require('../models');
 
 module.exports = {
   create: (body) => new Post(body),
+  addPostToUser: (post, user) => {
+    user.posts.push(post);
+    return user.save();
+  },
+  findOneByIdInUser: (idPost, user) => user.posts.id(idPost),
+  updateOneByIdInUser: (idPost, user, body) => {
+    const updatedPosts = user.posts.map((post) => {
+      if (post._id.toString() === idPost) {
+        const updatedPost = Object.assign(post, body);
+        return updatedPost;
+      }
+      return post;
+    });
+    // eslint-disable-next-line no-param-reassign
+    user.posts = updatedPosts;
+    return user.save();
+  },
 };
