c4e5e4f918e1f1df2b559e95bd28d5231a0a5c8a
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-07-10 20:02:49 -0600

    agregar cors(configuracion faltante para despliegue en produccion)

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const { errors } = require('celebrate');
 
 const api = express();
 const PORT = process.env.PORT || 3000;
 
+api.use(cors());
 api.use(morgan('dev'));
 api.use(express.urlencoded({ extends: true }));
 api.use(express.json({ extends: true }));
